efd484553c078c58a5a55b0ad705d795d299dcf8
DinaaGamal <dgwork77@gmail.com>
2019-10-26 15:44:56 +0200

    test commit

--- todo-clientSide/src/components/TodoList/TodoList.js
+++ todo-clientSide/src/components/TodoList/TodoList.js
@@ -1,30 +1,30 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchTodos } from '../../store/actions/fetchTodos';
-import TodoItem from '../TodoItem/TodoItem';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTodos } from "../../store/actions/fetchTodos";
+import TodoItem from "../TodoItem/TodoItem";
 
 function TodoList() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	/////////////useEffect hook/ #component did mountreplacement////////////////
-	useEffect(() => {
-		getAllTodos();
-	}, []);
+  /////////////useEffect hook/ #component did mountreplacement////////////////
+  useEffect(() => {
+    getAllTodos();
+  }, []);
 
-	const getAllTodos = () => {
-		dispatch(fetchTodos());
-	};
-	/////////this expression instead of below one/////////////
-	// mapStateToProps = (state) => {
-	//  return {todos: state.fetchTodosReducer}
-	// }
-	const todos = useSelector((state) => {
-		return state.fetchTodosReducer;
-	});
+  const getAllTodos = () => {
+    dispatch(fetchTodos());
+  };
+  /////////this expression instead of below one/////////////
+  // mapStateToProps = (state) => {
+  //  return {todos: state.fetchTodosReducer}
+  // }
+  const todos = useSelector(state => {
+    return state.fetchTodosReducer;
+  });
 
-	todos.map((todo) => {
-		return <TodoItem />;
-	});
+  return todos.map(todo => {
+    return <TodoItem />;
+  });
 }
 
 export default TodoList;
--- todo-clientSide/src/store/actions/fetchTodos.js
+++ todo-clientSide/src/store/actions/fetchTodos.js
@@ -1,12 +1,12 @@
-import { FETCH_TODOS } from './types';
-import axios from 'axios';
-import handleErrors from './handleErrors';
+import { FETCH_TODOS } from "./types";
+import axios from "axios";
+import handleErrors from "./handleErrors";
 
-export const fetchTodos = async (dispatch) => {
-	try {
-		const res = await axios.get('/todoApp/todos');
-		dispatch({ type: FETCH_TODOS, payload: res.data });
-	} catch (error) {
-		handleErrors(error);
-	}
+export const fetchTodos = () => async dispatch => {
+  try {
+    const res = await axios.get("/todoApp/todos");
+    dispatch({ type: FETCH_TODOS, payload: res.data });
+  } catch (error) {
+    handleErrors(error);
+  }
 };
